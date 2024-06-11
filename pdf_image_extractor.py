4d336aaed40377614ed7bce3be0ef07a9804b7a8
BrunoFelalaga <46535655+BrunoFelalaga@users.noreply.github.com>
2024-06-11 12:27:24 -0500

    Update pdf_image_extractor.py
    edits from Anna

--- pdf_image_extractor.py
+++ pdf_image_extractor.py
@@ -16,15 +16,14 @@ def delete_all_temporary_dirs():
 
 
 def bulk_mode(): 
-    file_paths = [os.path.join(BULK_PATH, f) for f in os.listdir(BULK_PATH) if os.path.isfile(os.path.join(BULK_PATH, f))]
-    for pdf_path in file_paths:
+    file_paths = [os.path.join(BULK_PATH, f) for f in os.listdir(BULK_PATH) if os.path.isfile(os.path.join(BULK_PATH, f)) and not f.startswith('.')]
+    for pdf_path in file_paths:    
         print(f'Working on {pdf_path}')
         delete_all_temporary_dirs()
     
         convert_pdf_to_images(pdf_path)
         convert_to_binary()
         process_images_in_folder()
-        
         
         num_pages = len(os.listdir(PDF_PAGES))
 
@@ -51,7 +50,7 @@ def bulk_mode():
             item_path = os.path.join(EXTRACTED_IMAGES_DIR, item)
             if not os.path.exists('final_output'):
                 os.makedirs('final_output')
-            output_item_path = os.path.join('final_output', os.path.splitext(os.path.split(pdf_path)[1])[0] + '_' + item)
+            output_item_path = os.path.join('final_output', os.path.splitext(pdf_path.split('/')[-1])[0] + '_' + item)
             if os.path.isfile(item_path):
                 shutil.copy2(item_path, output_item_path)
             elif os.path.isdir(item_path):
